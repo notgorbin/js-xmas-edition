ca8f7452837a2b0db899c82a20d54522ce6fa941
notgorbin <lukasmassaccesi@gmail.com>
2024-04-26 17:00:32 -0300

    Tarea 8

--- js/pruebas.js
+++ js/pruebas.js
@@ -10,6 +10,12 @@ function probarValidarNombre() {
       'Este campo debe tener menos de 50 caracteres',
       'Validar nombre no validó que el nombre sea menor a 50 caracteres',
   );
+
+  console.assert(
+    validarDescripcionRegalo(",.,.,.,.") ===
+      "El campo descripción solo puede tener números y letras",
+    "La función validar descripción regalo no validó que fuera solo números y letras"
+  );
 }
 
 probarValidarNombre();
